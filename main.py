93c52c8b28adbc1791a619535feeee6fc5dbf044
Damikers <154735956+Damikers@users.noreply.github.com>
2024-02-27 09:28:29 +0000

    7

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def uncharted(x):
   if x >= 2:
     print("Hello")
+  else:
+    print("Goodbye")
 
 print("Daily Commit 1")
 print("Daily Commit 2")
@@ -8,6 +10,8 @@ print("Daili Commit 3")
 print("Daily Commit 4")
 print("Daily Commit 5")
 print("Daily Commit 6")
+print("Daily Commit 7")
 
 uncharted(3)
 uncharted(2)
+uncharted(1)
